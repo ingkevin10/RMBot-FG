47731a4b0c0a46220ebc2876d2a747b7ed72b164
ingkevin10 <166031047+ingkevin10@users.noreply.github.com>
2025-03-06 08:42:55 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url' 
 
 global.owner = [
-  ['5491168352204', 'FG', true],
-  ['5493794297363'],
-  ['59172945992']
+  ['51906684493', 'FG', true],
+  [''],
+  ['']
 ] //Numeros de owner 
 
 global.mods = [''] 
-global.prems = ['50489079501', '573143917092']
+global.prems = ['51906684493', '51968007784']
 global.botNumber = [''] 
 global.APIs = { // API Prefix
   // name: 'https://website' 
